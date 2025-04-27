bd6aaa4f6877a7c22612b30742f29a620db3dd7e
Mika7596 <mika7596@gmail.com>
2025-04-27 23:23:57 +0200

    Added header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './header.css'
+
+function Header() {
+  return (
+    <div className='header'><h1>Aconsegueix la millor qualitat</h1></div>
+  )
+}
+
+export default Header
