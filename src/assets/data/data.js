b711b573edc3bf94dde62a05ef8655a8aa2da981
Mika7596 <mika7596@gmail.com>
2025-04-27 23:50:09 +0200

    Created a data file to import info when creating cards automatically

---
+++ src/assets/data/data.js
@@ -0,0 +1,20 @@
+export const data = [
+    {
+        title: 'Seo',
+        id: 1,
+        description: "Programació d'una web responsive completa",
+        price: 300
+    },
+    {
+        title: 'Ads',
+        id: 2,
+        description: "Programació d'una web responsive completa",
+        price: 400
+    },
+    {
+        title: 'Web',
+        id: 3,
+        description: "Programació d'una web responsive completa",
+        price: 500
+    }
+]
