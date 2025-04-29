64e4493de5e54caf49803467c427b5a452bafe24
Mika7596 <mika7596@gmail.com>
2025-04-29 11:23:16 +0200

    Wrapped the service cards in a form tag and created a list where the selected items will be saved.

--- src/components/organisms/services/Services.jsx
+++ src/components/organisms/services/Services.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
 import {data} from '../../../assets/data/data'
 import Card from '../../atoms/card/Card'
+import './services.css'
 
 function Services() {
-
+  let total = 0;
+  function onSubmit(e){
+    e.preventDefault();
+    const {checkbox1, checkbox2, checkbox3} = e.target
+    const checkboxList = [[checkbox1, 1], [checkbox2, 2], [checkbox3,3]]
+    const selectedServices = [];
+    checkboxList.map(item => {
+      item[0].checked && selectedServices.push(item[1])
+    })
+    console.log(selectedServices)
+  }
   const cards = data.map((item, i) =>{
-    
     return <Card key={i} data={item}></Card>
   })
+
   return (
     <>
+    <form onSubmit={onSubmit}>
     {cards}
-    hola
+    <div className='col-10'><h3 className='text-end'>Total = {total}</h3></div>
+    <button type='submit'>Submit</button>
+    </form>
     </>
   )
 }
