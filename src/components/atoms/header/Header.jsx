9b7bb467dbcadc800a00ea4d877c08588cd5933b
Mika7596 <mika7596@gmail.com>
2025-05-09 17:25:40 +0200

    Created Home page

--- src/components/atoms/header/Header.jsx
+++ src/components/atoms/header/Header.jsx
@@ -3,7 +3,7 @@ import './header.css'
 
 function Header() {
   return (
-    <div className='header'><h1>Aconsegueix la millor qualitat</h1></div>
+    <header className='header'><h1>Aconsegueix la millor qualitat</h1></header>
   )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './home.css'
+import Header from '../../components/atoms/header/Header'
+import Services from '../../components/organisms/services/Services'
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+    <Header></Header>
+    <main>
+      <form action="">
+      <Services></Services>
+    
+      </form>
+    </main>
+    </>
   )
 }
 
