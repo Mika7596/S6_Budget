74259e4b082763b5291be68f68ec0119d744136f
Mika7596 <mika7596@gmail.com>
2025-05-10 13:08:02 +0200

    Created Footer component

---
+++ src/components/atoms/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+        <p className='p-5 text-end'>tots els drets reservats</p>
+    </footer>
+  )
+}
+
+export default Footer
