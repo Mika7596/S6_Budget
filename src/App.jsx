8e7a57eb8fcfb5c5d8360e2dfd325edbd7652ac8
Mika7596 <mika7596@gmail.com>
2025-05-10 11:14:47 +0200

    Installed React-router-dom and set onclick function of the navigation button on welcome page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import './App.css'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import WelcomePage from './pages/welcomePage/WelcomePage'
 import Home from './pages/home/Home'
 
 function App() {
 
   return (
     <>
-    <Home></Home>
+    <Router>
+      <Routes>
+        <Route path='/' element={<WelcomePage/>}/>
+        <Route path='/calculator' element={<Home/>}/>
+        {/* <Route path='*' element={<NotFound/>}/> */}
+      </Routes>
+    </Router>
     
     </>
   )
